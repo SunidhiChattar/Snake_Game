f47139b9790346dbdaa8c3febec7a98da3a00e07
Sunidhi <chattarsunidhi@gmail.com>
2022-10-24 17:54:47 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        gameFrame frame = new gameFrame();
+
+        // write your code here
+    }
+}
---
+++ src/gameFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class gameFrame extends JFrame {
+    gameFrame(){
+        gamePanel panel = new gamePanel();
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true );
+        this.setLocationRelativeTo(null);
+
+
+    }
+}
